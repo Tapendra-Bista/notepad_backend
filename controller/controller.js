d44505bf822f537b0f3507d9c0e7ff56cbabb54b
tapendrabistao1 <tapendrabista01@gmail.com>
2024-06-13 17:23:16 +0545

    end

--- controller/controller.js
+++ controller/controller.js
@@ -1,6 +1,6 @@
 import { json } from "express";
-import notepad_module from "../configuration/config.js"
-
+import notepad_module from "../configuration/config.js";
+// create new text
 const text_add = async (req, resp) => {
     const { text } = req.body;
 
@@ -29,17 +29,54 @@ const text_add = async (req, resp) => {
         return resp.status(406).json({ status: false, message: "Not Acceptable !" })
     }
 }
-
+// fetch text from database
 const text_fetch = async (req, resp) => {
-    var data =  await notepad_module.find().sort({ "createdAt": -1 })
+    var data = await notepad_module.find().sort({ "createdAt": -1 })
     if (data) {
         console.log(data)
         return resp.status(200).json(data);
     } else {
         console.log("No items")
         return resp.status(404), json({ message: "NO items " })
-
     }
 }
 
-export { text_add, text_fetch };
+// delete text 
+const text_delete = async (req, resp) => {
+    const { id } = req.body
+    try {
+
+        const find = await notepad_module.findById(id)
+        if (find) {
+            const data_delete = await notepad_module.findOneAndDelete(id)
+            return resp.status(200).json({ status: true, message: "Deleted", data: data_delete })
+        }
+    } catch (error) {
+        console.log("Not found that text")
+        return resp.status(404).json({ status: false, message: 'Not found by id : ' + id })
+    }
+
+}
+
+// update text 
+const text_update = async (req, resp) => {
+    const { id, text } = req.body
+    try {
+        const find = await notepad_module.findById(id)
+        if (find) {
+            const data_update = await notepad_module.updateOne(
+                {
+                    $set: {
+                        notepadText: text,
+                    }
+                })
+            return resp.status(200).json({ status: true, message: "Updated", data: data_update })
+        }
+    } catch (error) {
+        console.log("error at update :" + error);
+        return resp.status(404).json({ status: false, message: 'Not found by id : ' + id })
+    }
+}
+export { text_add, text_delete, text_fetch, text_update };
+
+// end
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import  {text_add, text_fetch} from "../controller/controller.js";
+import  {text_add, text_delete, text_fetch, text_update} from "../controller/controller.js";
 const routes = express.Router();
-
 routes.route("/add").post(text_add)
 routes.route("/fetch").get(text_fetch)
+routes.route("/delete").delete(text_delete)
+routes.route("/update").patch(text_update)
 export default routes
 
